b7675e60474a6162c89be2815d071756fa3282f9
Ittipat Pattum <nampu@finema.co>
2025-05-21 13:33:19 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/screens/onboardings/TermsScreenFragment.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/screens/onboardings/TermsScreenFragment.kt
@@ -2,13 +2,15 @@ package co.finema.thaidotidbyfinema.screens.onboardings
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import co.finema.thaidotidbyfinema.R
 
 class TermsScreenFragment : Fragment(R.layout.fragment_terms_screen) {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    //    view.findViewById<Button>(R.id.btn_skip).setOnClickListener {
-    //      findNavController().navigate(R.id.action_onboardFragment_to_termsScreenFragment)
-    //    }
+    view.findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
+      findNavController().popBackStack()
+    }
   }
 }
